99833c19d54eb2ec6071f7b40bb6f7b428da1678
kakamal <kamal.kamal@soprasteria.com>
2021-06-17 00:28:37 +0530

    adding travis ci file

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,15 @@
 docker build -t chaudhary1993/multi-client:latest -t chaudhary1993/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t chaudhary1993/multi-server:latest -t chaudhary1993/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t chaudhary1993/multi-worker:latest  -t chaudhary1993/multi-worker:$SHA  -f ./worker/Dockerfile ./worker
+docker build -t chaudhary1993/multi-worker:latest -t chaudhary1993/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push chaudhary1993/multi-client:latest
 docker push chaudhary1993/multi-server:latest
 docker push chaudhary1993/multi-worker:latest
-docker push chaudhary1993/multi-client:$SHA 
-docker push chaudhary1993/multi-server:$SHA 
+
+docker push chaudhary1993/multi-client:$SHA
+docker push chaudhary1993/multi-server:$SHA
 docker push chaudhary1993/multi-worker:$SHA
+
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=chaudhary1993/multi-server:$SHA
 kubectl set image deployments/client-deployment client=chaudhary1993/multi-client:$SHA
